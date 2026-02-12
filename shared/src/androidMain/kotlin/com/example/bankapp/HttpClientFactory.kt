59ee6f1a794ac43a51ce987760edff83e0b9fe87
aymennegra <aymen.negra@esprit.tn>
2026-02-12 13:44:06 +0100

    refactor

---
+++ shared/src/androidMain/kotlin/com/example/bankapp/HttpClientFactory.kt
@@ -0,0 +1,22 @@
+package com.example.bankapp
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+actual object HttpClientFactory {
+    actual fun create(): HttpClient {
+        return HttpClient(OkHttp) {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    isLenient = true
+                    prettyPrint = true
+                })
+            }
+        }
+    }
+}
--- shared/src/androidMain/kotlin/com/example/bankapp/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/example/bankapp/Platform.android.kt
@@ -1,36 +1,8 @@
 package com.example.bankapp
 
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.DEFAULT
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
-
 class AndroidPlatform : Platform {
     override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
 
-actual object HttpClientFactory {
-    actual fun create(): HttpClient {
-        return HttpClient(OkHttp) {
-            install(ContentNegotiation) {
-                json(Json {
-                    ignoreUnknownKeys = true
-                    isLenient = true
-                    prettyPrint = true
-                })
-            }
-
-            install(Logging) {
-                logger = Logger.DEFAULT
-                level = LogLevel.ALL
-            }
-        }
-    }
-}
